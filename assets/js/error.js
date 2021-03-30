c39949919365b09e24d1a476ac1990341907614a
jay-o-sullivan <jayosullivan25@gmail.com>
2021-03-30 17:13:25 +0000

    added different image , styles and alert

---
+++ assets/js/error.js
@@ -0,0 +1,20 @@
+
+function showalert(){
+ 
+var optsel = confirm("Do you want to return to previous page?");
+ 
+if (optsel == true){
+ 
+    alert("You clicked OK!");
+ 
+  }
+ 
+else{
+ 
+    alert("You clicked Cancel!");
+ 
+}   
+ 
+}
+ 
+ 
