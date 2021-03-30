383e27da95b9c320f95455d54a491b784e9785d5
jay-o-sullivan <jayosullivan25@gmail.com>
2021-03-30 17:15:30 +0000

    added animation buttons, styled them

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,3 +51,64 @@ function sendMail(contactForm) {
     );
     return false;  // To block from loading a new page
 }
+
+$(document).ready(function(){
+
+/*! Fades in page on load */
+$('h1').css('display', 'none');
+$('h1').fadeIn(1500);
+
+$('#p1').css('display', 'none');
+$('#p1').fadeIn(1500);
+
+});
+// all icons shake when button is clicked
+$('#shake').click(function() {
+  shake($('.col-sm-4'));
+});
+
+// all icons bounce when button is clicked
+$('#bounce').click(function() {
+  bounce($('.col-sm-4'));
+});
+
+// adaptable SHAKE function, from 
+// https://bradleyhamilton.com/projects/shake/index.html 
+function shake(thing) {
+  var interval = 100;
+  var distance = 10;
+  var times = 6;
+
+  for (var i = 0; i < (times + 1); i++) {
+    $(thing).animate({
+      left:
+        (i % 2 == 0 ? distance : distance * -1)
+    }, interval);
+  }
+  $(thing).animate({
+    left: 0,
+    top: 0
+  }, interval);
+}
+// end SHAKE
+
+// adaptable BOUNCE function, adapted from 
+// http://stackoverflow.com/questions/10363671/jquery-bounce-effect-on-click-no-jquery-ui 
+// code by "christian" 
+function bounce(thing) {
+  var interval = 100;
+  var distance = 20;
+  var times = 6;
+  var damping = 0.8;
+
+  for (var i = 0; i < (times + 1); i++) {
+    var amt = Math.pow(-1, i) * distance / (i * damping);
+    $(thing).animate({
+      top: amt
+    }, 100);
+  }
+  $(thing).animate({
+    top: 0
+  }, interval);
+}
+// end BOUNCE
