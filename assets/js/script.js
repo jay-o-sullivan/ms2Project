a751f0310cdc3601ec515c23cb2189bcdad81ea7
jay-o-sullivan <jayosullivan25@gmail.com>
2021-04-02 00:37:30 +0000

    fixed the api error with map

--- assets/js/script.js
+++ assets/js/script.js
@@ -164,3 +164,18 @@ function init() {
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
+// Initialize and add the map
+      function initMap() {
+        // The location of limerick
+        const limerick = { lat: 52.668018, lng: -8.630498 };
+        // The map, centered at limerick
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 4,
+          center: limerick,
+        });
+        // The marker, positioned at limerick
+        const marker = new google.maps.Marker({
+          position: limerick,
+          map: map,
+        });
+      }
